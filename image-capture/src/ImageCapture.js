cf19edf5e9e9a6ac657111dcf2ea696681a1bc30
Admin <admin@Suresh-Villuri-Macbook-Pro.local>
2020-05-16 20:20:38 -0700

    Added error scenario

--- image-capture/src/ImageCapture.js
+++ image-capture/src/ImageCapture.js
@@ -1,22 +1,30 @@
-import React, { useEffect, useRef, useState, useMemo } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const ImageCapture = React.memo(() => {
   const videoRef = useRef();
   const snapshotImage = useRef();
-  const [videoData, setVideoData] = useState({ active: false });
+  const [videoData, setVideoData] = useState({
+    active: false,
+    error: '',
+  });
 
   useEffect(() => {
     const fetchCameraData = async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: { deviceId: '' },
-      });
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: { deviceId: '' },
+        });
 
-      videoRef.current.srcObject = stream;
-      setVideoData({ ...videoData, active: stream.active });
-      videoRef.current.play();
+        videoRef.current.srcObject = stream;
+        setVideoData({ ...videoData, active: stream.active });
+        videoRef.current.play();
+        console.log(stream);
+      } catch (e) {
+        console.log('error fetching data');
+        setVideoData({ ...videoData, error: 'error fetching data' });
+      }
     };
-
     if (!videoData.active) {
       fetchCameraData();
     }
@@ -25,13 +33,20 @@ const ImageCapture = React.memo(() => {
   const captureImage = () => {
     const getSnapshot = snapshotImage.current.getContext('2d');
 
-    getSnapshot.drawImage(videoRef, 0, 0);
+    getSnapshot.drawImage(videoRef, 0, 0, 680, 360);
   };
 
+  console.log(videoData.active);
   return (
     <div className='video-capture'>
-      <video width='100%' height='700' ref={videoRef} />
-      <button onClick={captureImage}>Capture Image</button>
+      {!videoData.error ? (
+        <>
+          <video width='100%' height='700' ref={videoRef} />
+          <button onClick={captureImage}>Capture Image</button>
+        </>
+      ) : (
+        <p class='error'>{videoData.error}</p>
+      )}
 
       <div className='snapshot-image'>
         <canvas width='100%' height='700' ref={snapshotImage} />
